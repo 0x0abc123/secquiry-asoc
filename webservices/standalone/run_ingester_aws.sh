fb1aa7d68059dde1f79c1241c4c646de1a3ca12d
0x0abc123 <v3natoris@gmail.com>
2022-12-29 09:51:55 +1100

    Commit 1672267914

--- webservices/standalone/run_ingester_aws.sh
+++ webservices/standalone/run_ingester_aws.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # usage:  run_ingester_aws.sh http://<collablio_host:port> <s3bucketname>
-cd ..
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/..
 python3 -m standalone.ingester_aws $1 $2
